2aa849011625b7d83b9d67ee6badd6694abb7852
badrisinghoo7 <you@example.com>
2025-06-12 16:21:46 +0530

    make some changes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { UserModel } = require('../models/User');
+const { UserModel } = require('../models/user');
 require('dotenv').config();
 const UserRouter = express.Router();
 
